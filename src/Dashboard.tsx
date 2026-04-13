4a378b91e57bbbe226392436500c2704be776663
luciano-antonio <luciano.a.antonio@gmail.com>
2026-04-12 22:01:14 -0300

    feat: hover nas imagens e auto update na recently played

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -41,6 +41,7 @@ function Dashboard()    {
 
                                                                                 // busca musicas recentes
             useEffect(() => {
+               const buscar = () => { 
                 fetch(`${import.meta.env.VITE_API_URL}/recently-played`, {
                     headers: {
                         'Authorization': `Bearer ${localStorage.getItem('token')}`        // buscar musicas recentes. Codigo funcional mas não está sendo utilizado
@@ -49,36 +50,65 @@ function Dashboard()    {
                     .then(res => res.json())
                     .then(data => { setRecently(Array.isArray(data) ? data: [])
                     })
+
+               }
+                
+               buscar()
+               const intervalo = setInterval(buscar,30000)
+
+               return () => clearInterval(intervalo)
             }, [])
 
     return (
         <div className="min-h-screen bg-[#0a0a0a]">    
-            <div className=" px-8 py-6 overflow-hidden text-white max-w-3xl mx-auto">
-                <h1 className="mb-10 text-3xl font-bold mb-8">Dashboard</h1>
+            <div className=" px-8 py-12 overflow-hidden text-white max-w-3xl mx-auto px-16">
 
-            <section onClick={() => navigate('/top-tracks')} className="... cursor-pointer mb-8" >
-                <h2 className="text-xl font-semibold mb-3">Top Musicas</h2>
-                <div className="flex gap-4">
+                <h1 className="mb-10 text-4xl font-bold text-center mb-12">Dashboard</h1>
+
+            <section onClick={() => navigate('/top-tracks')} className="... cursor-pointer mb-12" >
+                <h2 className="text-2xl font-semibold mb-3">Top Musicas</h2>
+                <div className="flex gap-4 overflow-visible">
                     {tracks.slice(0, 5).map((track: any) => (
-                        <img key={track.ranking} src={track.imagem || 'https://placehold.co/96'} className="w-24 h-24 rounded-lg object-cover" />
+                        <div key={track.imagem} className="relative group cursor-pointer hover:scale-110 transition-transform duration-200">
+                            <img src={track.imagem || 'https://placehold.co/96'} className="w-28 h-28 rounded-lg object-cover" />
+                            <div className="absolute buttom-0 left-0 right-0 bg-black/70 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                                <p className="text-white text-xs truncate">{track.name}</p>
+                                <p className="text-white text-xs truncate">{track.artista}</p>
+                                
+                            </div>
+                         </div>
                     ))}
                 </div>
             </section>
 
-            <section onClick={() => navigate('/top-artists')} className="... cursor-pointer mb-8" >
-                <h2 className="text-xl font-semibold mb-3">Top Artistas</h2>
-                    <div className="flex gap-4"> 
+            <section onClick={() => navigate('/top-artists')} className="... cursor-pointer mb-12" >
+                <h2 className="text-2xl font-semibold mb-3">Top Artistas</h2>
+                    <div className="flex gap-4 overflow-visible"> 
                         {artists.slice(0,5).map((artists: any) => (
-                            <img key={artists.ranking} src={artists.imagemArtists || 'https://placehold.co/96'} className="w-24 h-24 rounded-lg object-cover" />
+                            <div key={artists.imagemArtists} className="relative group cursor-pointer hover:scale-110 transition-transform duration-200">
+                            <img src={artists.imagemArtists || 'https://placehold.co/96'} className="w-28 h-28 rounded-lg object-cover" />
+                            <div className="absolute buttom-0 left-0 right-0 bg-black/70 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                                <p className="text-white text-xs truncate">{artists.name}</p>
+                                <p className="text-white text-xs truncate">{artists.artista}</p>
+                                
+                            </div>
+                         </div>
                         ))}
                     </div>
             </section>
 
-            <section onClick={() => navigate('/recently-played')} className="... cursor-pointer mb-8">
-                <h2 className="text-xl font-semibold mb-3">Tocadas Recentemente</h2>
-                   <div className="flex gap-4">
+            <section onClick={() => navigate('/recently-played')} className="... cursor-pointer mb-12">
+                <h2 className="text-2xl font-semibold mb-3">Tocadas Recentemente</h2>
+                   <div className="flex gap-4 overflow-visible">
                     {recently.slice(0,5).map((recently: any) => (
-                        <img key={recently.ranking} src={recently.recentlyImages || 'https://placehold.co/96'} className="w-24 h-24 rounded-lg object-cover" />
+                        <div key={recently.recentlyImages} className="relative group cursor-pointer hover:scale-110 transition-transform duration-200">
+                            <img src={recently.recentlyImages || 'https://placehold.co/96'} className="w-28 h-28 rounded-lg object-cover" />
+                            <div className="absolute buttom-0 left-0 right-0 bg-black/70 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                                <p className="text-white text-xs truncate">{recently.name}</p>
+                                <p className="text-white text-xs truncate">{recently.artista}</p>
+                                
+                            </div>
+                         </div>
                     ))}
                     </div>
             </section>

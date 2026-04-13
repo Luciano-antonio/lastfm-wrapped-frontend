fa490e83b416338b4f3e30371eb8ff7eeff54dd4
luciano-antonio <luciano.a.antonio@gmail.com>
2026-04-13 00:01:33 -0300

    feat: add skeleton e fade-in

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -60,18 +60,25 @@ function Dashboard()    {
             }, [])
 
     return (
-        <div className="min-h-screen bg-[#0a0a0a]">    
+        <div className="min-h-screen bg-[#0f0f13]">    
             <div className=" px-8 py-12 overflow-hidden text-white max-w-3xl mx-auto px-16">
 
                 <h1 className="mb-10 text-4xl font-bold text-center mb-12">Dashboard</h1>
 
-            <section onClick={() => navigate('/top-tracks')} className="... cursor-pointer mb-12" >
+            <section onClick={() => navigate('/top-tracks')} className="... cursor-pointer mb-12 ... fade-in" >
                 <h2 className="text-2xl font-semibold mb-3">Top Musicas</h2>
-                <div className="flex gap-4 overflow-visible">
+                {tracks.length === 0 ? (
+                    <div className="flex gap-4">
+                        {Array.from({length: 5}).map((_, index) => (
+                            <div key={index} className="w-28 h-28 rounded-lg bg-white/10 animate-puls" />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="flex gap-4 overflow-visible">
                     {tracks.slice(0, 5).map((track: any) => (
                         <div key={track.imagem} className="relative group cursor-pointer hover:scale-110 transition-transform duration-200">
                             <img src={track.imagem || 'https://placehold.co/96'} className="w-28 h-28 rounded-lg object-cover" />
-                            <div className="absolute buttom-0 left-0 right-0 bg-black/70 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                            <div className="absolute buttom-0 left-0 right-0 bg-zinc-800/40 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
                                 <p className="text-white text-xs truncate">{track.name}</p>
                                 <p className="text-white text-xs truncate">{track.artista}</p>
                                 
@@ -79,15 +86,26 @@ function Dashboard()    {
                          </div>
                     ))}
                 </div>
+                )}
+                
             </section>
 
-            <section onClick={() => navigate('/top-artists')} className="... cursor-pointer mb-12" >
+            <section onClick={() => navigate('/top-artists')} className="... cursor-pointer mb-12 ... fade-in" >
                 <h2 className="text-2xl font-semibold mb-3">Top Artistas</h2>
-                    <div className="flex gap-4 overflow-visible"> 
+                    
+                    {artists.length === 0 ? (
+                        <div className="flex gap-4">
+                            {Array.from({length: 5}).map((_, index) => (
+                                <div key={index} className="w-28 h-28 rounded-lg bg-white/10 animate-puls" />
+                            ))}
+                        </div>
+                    ) : (
+
+                        <div className="flex gap-4 overflow-visible"> 
                         {artists.slice(0,5).map((artists: any) => (
                             <div key={artists.imagemArtists} className="relative group cursor-pointer hover:scale-110 transition-transform duration-200">
                             <img src={artists.imagemArtists || 'https://placehold.co/96'} className="w-28 h-28 rounded-lg object-cover" />
-                            <div className="absolute buttom-0 left-0 right-0 bg-black/70 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                            <div className="absolute buttom-0 left-0 right-0 bg-zinc-800/40 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
                                 <p className="text-white text-xs truncate">{artists.name}</p>
                                 <p className="text-white text-xs truncate">{artists.artista}</p>
                                 
@@ -95,15 +113,27 @@ function Dashboard()    {
                          </div>
                         ))}
                     </div>
+                    )}
+                    
+                    
             </section>
 
-            <section onClick={() => navigate('/recently-played')} className="... cursor-pointer mb-12">
+            <section onClick={() => navigate('/recently-played')} className="... cursor-pointer mb-12 ... fade-in">
                 <h2 className="text-2xl font-semibold mb-3">Tocadas Recentemente</h2>
-                   <div className="flex gap-4 overflow-visible">
+                   
+                   {recently.length === 0 ? (
+                    <div className="flex gap-4">
+                        {Array.from({length: 5}).map((_, index) => (
+                            <div key={index} className="w-28 h-28 rounded-lg bg-white/10 animate-plus" />
+                        ))}
+                    </div>
+                   ) : (
+                        
+                        <div className="flex gap-4 overflow-visible">
                     {recently.slice(0,5).map((recently: any) => (
                         <div key={recently.recentlyImages} className="relative group cursor-pointer hover:scale-110 transition-transform duration-200">
                             <img src={recently.recentlyImages || 'https://placehold.co/96'} className="w-28 h-28 rounded-lg object-cover" />
-                            <div className="absolute buttom-0 left-0 right-0 bg-black/70 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                            <div className="absolute buttom-0 left-0 right-0 bg-zinc-800/40 rounded-b-lg p-1 opacity-0 group-hover:opacity-100 transition-opacity">
                                 <p className="text-white text-xs truncate">{recently.name}</p>
                                 <p className="text-white text-xs truncate">{recently.artista}</p>
                                 
@@ -111,6 +141,8 @@ function Dashboard()    {
                          </div>
                     ))}
                     </div>
+                   )}
+                           
             </section>
             </div>
         </div>
